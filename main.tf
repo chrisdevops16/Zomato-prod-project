27d9989d7d17edd65264e22d51a755380e5b2dff
luizchris <luizchris23@gmail.com>
2023-07-15 16:34:37 +0000

    terraform tfstae added in s3 bucket

--- main.tf
+++ main.tf
@@ -18,7 +18,13 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
@@ -36,3 +42,5 @@ resource "aws_security_group" "sg" {
   }
 
 }
+
+
