c28173b5f46c229e765b92d17ad3291500f190ca
luizchris23 <luizchris23@gmail.com>
2023-07-15 13:46:30 +0000

    sg for webserver added

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {}
+
+resource "aws_security_group" "sg" {
+  name_prefix = "${var.project_name}-${var.project_env}-"
+  description = "Allow ssh,http"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name"    = "${var.project_name}-${var.project_env}",
+    "project" = var.project_name,
+    "env"     = var.project_env
+  }
+
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "project_name" {
+  default = "Zomato"
+}
+
+variable "project_env" {
+  default = "Prod"
+}
